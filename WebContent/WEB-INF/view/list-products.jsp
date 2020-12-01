e391528fd809a90c223368e2767cc8fcfcb9c881
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-12-01 16:28:08 +0100

    Testing Spring MVC Controller

---
+++ WebContent/WEB-INF/view/list-products.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+
+   <head>
+      <title>List of Products</title>
+   </head>
+   
+   <body>
+      List of Products - coming up ...
+   </body>
+
+</html>
---
+++ src/com/mirjalolcode/springdemo/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.mirjalolcode.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+	@RequestMapping("/list")
+	public String listProducts(Model theModel) {
+		return "list-products";
+	}
+}
