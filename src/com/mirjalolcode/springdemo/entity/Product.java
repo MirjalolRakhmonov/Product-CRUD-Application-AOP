a67cf51f8e2630030e49d8dfef884214bf7de9bc
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-12-01 16:43:40 +0100

    Product class is implemented in entity package

---
+++ src/com/mirjalolcode/springdemo/entity/Product.java
@@ -0,0 +1,70 @@
+package com.mirjalolcode.springdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "product_name")
+	private String productName;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "price")
+	private int price;
+	
+	public Product() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", productName=" + productName + ", description=" + description + ", price="
+				+ price + "]";
+	}
+	
+}
