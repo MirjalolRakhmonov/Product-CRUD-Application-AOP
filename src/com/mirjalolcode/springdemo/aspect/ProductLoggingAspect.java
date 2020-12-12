8b4a92a51c38c1745b64dc21f5f20b8be2225fb0
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-12-12 17:50:52 +0100

    @AfterReturning advice is added

--- src/com/mirjalolcode/springdemo/aspect/ProductLoggingAspect.java
+++ src/com/mirjalolcode/springdemo/aspect/ProductLoggingAspect.java
@@ -3,6 +3,7 @@ package com.mirjalolcode.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -35,9 +36,25 @@ public class ProductLoggingAspect {
 		String 	theMethod=theJoinPoint.getSignature().toShortString();
 		myLogger.info("=====>> in @Before: calling: "+theMethod);
 		
-		// display the arguments to the method
+		// get the arguments
+		Object[] args=theJoinPoint.getArgs();
+		
+		// loop through and display args
+		for(Object tempArg : args) {
+			myLogger.info("=====>> argument: "+tempArg);
+		}
+		
 	}
 
 	// add @AfterReturning advice
-	
+	@AfterReturning(pointcut = "forAppFlow()",
+			returning = "theResult")
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		// display method we are calling
+				String 	theMethod=theJoinPoint.getSignature().toShortString();
+				myLogger.info("=====>> in @Before: calling: "+theMethod);
+				
+		// display data returned
+	    myLogger.info("=====>> result: "+theResult);
+	}
 }
